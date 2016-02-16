2f626ab71e2bdb81a7ff1e41a4ff6ffef1b21bbd
Brian <brianmartinmurphy@gmail.com>
2016-02-16 22:08:05 +0000

    - Adding Pod files

---
+++ Example/Pods/Target Support Files/IceAndFireKit/IceAndFireKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_IceAndFireKit : NSObject
+@end
+@implementation PodsDummy_IceAndFireKit
+@end
---
+++ Example/Pods/Target Support Files/IceAndFireKit/IceAndFireKit-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double IceAndFireKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char IceAndFireKitVersionString[];
+
